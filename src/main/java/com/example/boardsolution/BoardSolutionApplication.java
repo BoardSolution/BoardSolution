4556d3437ca6fd0c3c20e182e9c0d462e1d14a02
eunsuk <eunsukkim84@gmail.com>
2023-10-17 18:18:02 +0900

    first commit

---
+++ src/main/java/com/example/boardsolution/BoardSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardSolutionApplication.class, args);
+    }
+
+}
